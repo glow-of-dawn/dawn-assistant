a82ed20fb9e67787f43cffd9085e680f2e5285f4
HFOREST-480S\hforest-480s <heartforests@gmail.com>
2026-02-08 23:02:30 +0800

    init
    init

---
+++ src/main/java/com/dawn/plugin/DawnAssistantApplication.java
@@ -0,0 +1,19 @@
+package com.dawn.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * [main]
+ * 创建时间：2025/03/07 14:37
+ *
+ * @author forest
+ */
+@SpringBootApplication
+public class DawnAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViviAssistantApplication.class, args);
+    }
+
+}
